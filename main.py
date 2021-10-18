1cfb06877f7d76bb0da4b042fd2fad6d23f5f8cb
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-10-18 20:38:01 +0200

    Completed exercise.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+respond = requests.get("https://api.npoint.io/4af156202f984d3464c3")
+result = respond.json()
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data=result)
+
+
+@app.route('/<int:id>')
+def posts(id):
+    for post in result:
+        if post["id"] == id:
+            return render_template("post.html", id=id, data=post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
